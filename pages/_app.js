299bf5358118fd8411b47b5631066118ccbf4a1e
aurakivibes <aurakacrystals@gmail.com>
2025-11-26 16:57:12 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,2 +1,10 @@
-import '../styles/globals.css'
-export default function App({Component,pageProps}){return <Component {...pageProps}/> }
+// app/page.js
+import Hero from '../components/Hero'
+
+export default function Page() {
+  return (
+    <main>
+      <Hero />
+    </main>
+  )
+}
