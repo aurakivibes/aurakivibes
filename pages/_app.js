70daee5be764ac6088558ba04cfc0c2f1f2cef37
aurakivibes <aurakacrystals@gmail.com>
2026-01-04 18:12:30 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,6 @@
-import "@/styles/globals.css";
+import "../styles/globals.css";
 
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
 
