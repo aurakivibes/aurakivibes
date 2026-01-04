98a43c233d338e7a77c1952139d8721ec50e9b29
aurakivibes <aurakacrystals@gmail.com>
2026-01-04 18:29:07 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,5 @@
-// pages/index.js
-import Head from 'next/head'
-import Hero from '../components/Hero'
+import Hero from "../components/Hero";
 
 export default function Home() {
-  return (
-    <>
-      <Head><title>Aura Ki Vibes</title></Head>
-      <Hero />
-    </>
-  )
+  return <Hero />;
 }
