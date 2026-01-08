d88785232a8c12902c9a5097938f679a5110c4cd
aurakivibes <aurakacrystals@gmail.com>
2026-01-08 19:58:34 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,1 +1,20 @@
-pages/_document.js
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        {/* FONT AWESOME CDN */}
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
+
