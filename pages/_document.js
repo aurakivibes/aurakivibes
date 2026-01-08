c5b2670ecf33252838be4f8de7b8ef32419b7e21
aurakivibes <aurakacrystals@gmail.com>
2026-01-08 20:14:04 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,9 @@ export default function Document() {
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+          integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4fZgW9nZ3z9r5z0z3z3z3z3z"
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
         />
       </Head>
       <body>
